f1d5659b3350fac8c59f420502d23fde675d9a4d
nikkou-0814 <125991634+nikkou-0814@users.noreply.github.com>
2025-01-02 03:22:12 +0900

    未使用を削除

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,43 +12,6 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      height: {
-        '100px': '100px',
-      },
-      animation: {
-        pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-        'fade-in-scale': 'fade-in-scale 1.5s forwards',
-        'fade-out-scale': 'fade-out-scale 0.5s forwards',
-        'bounce': 'bounce 2s ease-in-out infinite',
-      },
-      keyframes: {
-        pulse: {
-          '0%, 100%': { opacity: '1' },
-          '50%': { opacity: '0.5' },
-        },
-        'fade-in-scale': {
-          '0%': { opacity: '0'},
-          '100%': { opacity: '1'},
-        },
-        'fade-out-scale': {
-          '0%': { opacity: '1'},
-          '100%': { opacity: '0'},
-        },
-        'bounce': {
-          '0%, 100%': { transform: 'scale(1)' },
-          '50%': { transform: 'scale(0.7)' },
-        },
-      },
-      transitionTimingFunction: {
-        'custom': 'cubic-bezier(0.22, 1, 0.36, 1)',
-      },
-      transitionDuration: {
-        '400': '400ms',
-      },
-      transitionDelay: {
-        '200': '200ms',
-        '400': '400ms',
-      },
     },
   },
   plugins: [],
